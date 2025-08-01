bc850978c3e15639951a1aa55167854129f87baf
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2025-08-01 10:19:08 +0800

    added catch action (WORKING VERSION)

--- media/lua/client/SimpleSportMod_Catch.lua
+++ media/lua/client/SimpleSportMod_Catch.lua
@@ -1,205 +1,68 @@
---[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
-  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
-  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
-  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
-  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
-  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
-   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
-|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
-|                        				 Custom  PZ  Mod  Developer  for  Hire										                      		 |
-|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
-|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/			       				          |
-|                       		                                    														 	 		 	 		 	 |
-|                       	Discord:    glytch3r															     	     	     	     	     	          |
-|                       		                                    														 	                   |
-|                       	Support:    https://ko-fi.com/glytch3r														    	                   |
-|_______________________________________________________________________________________________________________________________-]]
---[[_____________________________________________________________________________________________________________________________
-   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
-  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
-  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
-  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
-  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
-  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
-   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
-█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
-
------------------------            ---------------------------
-
 SimpleSportMod = SimpleSportMod or {}
 
-
-
-function SimpleSportMod.getData(pl)   
-   pl = pl or getPlayer()
-   return pl:getModData()['SimpleSportMod']
-end
-
-function SimpleSportMod.isPreparedToCatch(pl)   
-   pl = pl or getPlayer()
-   local data = SimpleSportMod.getData(pl) or {}
-   return data['isPreparedToCatch']   
-end
-
-function SimpleSportMod.setCatchPrepare(pl, bool)   
-   pl = pl or getPlayer()
-   local data = SimpleSportMod.getData(pl) or {}
-   data['isPreparedToCatch'] = bool or not  SimpleSportMod.isPreparedToCatch(pl)  
-   if bool then
-      SimpleSportMod.setReachMarker(sq)
-      pl:playEmote("surrender")
-   else
-      pl:playEmote("idle")
-   end
-   return bool
-end
-
-
---[[ 
-function SimpleSportMod.cancelCatch(pl)
-   pl:getModData()['SimpleSportMod']['isPreparedToCatch'] = false   
-end
-Events.OnPlayerMove.Add(OnPlayerMove)
- ]]
-
-function SimpleSportMod.autoCatchHandler(pl)
-   pl = pl or getPlayer()
-   if not pl then return end 
-
-   if not SimpleSportMod.isPreparedToCatch(pl) then return end 
-
-   local csq = pl:getCurrentSquare()
-   if not csq then return end
-   
-   local item, ball , targSq  = SimpleSportMod.findSportItem(csq)
-   if not item then return end
-   
-   SimpleSportMod.instaCatch(pl, item)
-end
-Events.OnPlayerUpdate.Add(SimpleSportMod.autoCatchHandler)
------------------------            ---------------------------
- function SimpleSportMod.instaCatch(pl, item)
-
-      if not pl or not item then return end 
-   
-      if SimpleSportMod.isPreparedToCatch(pl)  then
-         if not pl:getCharacterActions():isEmpty() then
-            pl:StopAllActionQueue()
-         end
-      
-         ISTimedActionQueue.add(ISGrabItemAction:new(pl, item:getWorldItem(), 0));
-         ISTimedActionQueue.add(ISEquipWeaponAction:new(pl, item, 0, true));
-         SimpleSportMod.setCatchPrepare(pl, false)   
-      end
-end
-
-
---[[ function SimpleSportMod.instaCatch(pl, item)
-   pl = pl or getPlayer() 
-   local csq = pl:getCurrentSquare()
-   if not csq then return end
-   item = item or SimpleSportMod.getSportItem(csq)
-   if item then
-      if tostring(WeaponType.getWeaponType(pl)) == "barehand" then  
-         local fType = item:getFullType()
-         if fType then
-            if SimpleSportMod.SportItems[fType] then
-               if SimpleSportMod.isPreparedToCatch(pl)  then
-                  if not pl:getCharacterActions():isEmpty() then
-                     pl:StopAllActionQueue()
-                  end
-               
-                  ISTimedActionQueue.add(ISGrabItemAction:new(pl, item:getWorldItem(), 0));
-                  ISTimedActionQueue.add(ISEquipWeaponAction:new(pl, item, 0, true));
-                  SimpleSportMod.setCatchPrepare(pl, false)   
-               end
-            end
-         end
-      end
-   end
-end ]]
------------------------            ---------------------------
-
-
-function SimpleSportMod.setReachMarker(sq)
-   local rad = SandboxVars.SimpleSportMod.PickUpDistance or 3
-   local dur = SandboxVars.SimpleSportMod.CatchWindow or 3
-   local pl = getPlayer()
-   sq = sq or pl:getCurrentSquare() 
-   local ReachMarker = ReachMarker or getWorldMarkers():addGridSquareMarker("circle_highlight_2", "circle_highlight_2", sq, 0, 0, 0.31, true, rad)
-
-   timer:Simple(dur, function()
-      if ReachMarker then ReachMarker:remove() end
-   end)
-end
-
------------------------            ---------------------------
-function SimpleSportMod.instaCatchAction(pl)
-   pl = pl or getPlayer()
-   SimpleSportMod.setCatchPrepare(pl, true)
-   if tostring(WeaponType.getWeaponType(pl)) ~= "barehand" then return end
-   if not SimpleSportMod.isPreparedToCatch(pl) then return end
-
-   local dur = SandboxVars.SimpleSportMod.CatchWindow or 3
-   timer:Simple(dur, function()
-      SimpleSportMod.setCatchPrepare(pl, false)
-   end)
-   ISTimedActionQueue.add(ISWaitForCatchAction:new(pl))
-end
-
------------------------            ---------------------------
+-----------------------
 ISWaitForCatchAction = ISBaseTimedAction:derive("ISWaitForCatchAction")
 
 function ISWaitForCatchAction:isValid()
-   return SimpleSportMod.isPreparedToCatch(self.pl)
+   return tostring(WeaponType.getWeaponType(self.character)) == "barehand"
 end
 
 function ISWaitForCatchAction:start()
-   SimpleSportMod.setCatchPrepare(self.pl, true)
+   self:setAnimVariable("isCatchWaiting", true)
+   self:setActionAnim("WaitToCatch")
+   SimpleSportMod.setReachMarker()
 end
 
 function ISWaitForCatchAction:update()
-   local item = SimpleSportMod.getSportItem(self.pl:getCurrentSquare())
-   if item and SimpleSportMod.isSportItem(item) then
-      self.targetItem = item
+   local csq = self.character:getCurrentSquare() 
+   local item, ball, targSq  = SimpleSportMod.findSportItem(csq)
+   if item then      
+      self.ball = ball
+      local fType = SimpleSportMod.getFType(item)
+      local inv = self.character:getInventory()
+      self.targetItem = inv:AddItem(fType)
+      self.character:setPrimaryHandItem(self.targetItem)
+      ISRemoveItemTool.removeItem(item, self.character:getPlayerNum())
+      
       self:forceComplete()
    end
 end
 
 function ISWaitForCatchAction:perform()
-   if self.targetItem then
-      ISTimedActionQueue.add(ISGrabItemAction:new(self.pl, self.targetItem:getWorldItem(), 0))
-      ISTimedActionQueue.add(ISEquipWeaponAction:new(self.pl, self.targetItem, 0, true))
-   end
-   SimpleSportMod.setCatchPrepare(self.pl, false)
+--[[    if self.targetItem then
+      local item = self.targetItem
+      item:getWorldItem():removeFromSquare()
+      self.character:getInventory():AddItem(item)
+      self.character:setPrimaryHandItem(item)
+   end ]]
+   SimpleSportMod.clearCatchVar()
    ISBaseTimedAction.perform(self)
 end
 
+function ISWaitForCatchAction:animEvent(event, parameter)
+   if event == 'waitFinished' then
+      self:forceStop()
+   end
+end
+
 function ISWaitForCatchAction:forceStop()
-   SimpleSportMod.setCatchPrepare(self.pl, false) 
+   SimpleSportMod.clearCatchVar()
    ISBaseTimedAction.stop(self)
 end
 
-function ISWaitForCatchAction:new(pl)
-   local o = {}
-   setmetatable(o, self)
-   self.__index = self
-   o.pl = pl
+function ISWaitForCatchAction:stop()
+   SimpleSportMod.clearCatchVar()
+   ISBaseTimedAction.stop(self)
+end
+
+function ISWaitForCatchAction:new(character)
+   local o = ISBaseTimedAction.new(self, character)
+   o.character = character
    o.targetItem = nil
    local t = SandboxVars.SimpleSportMod.CatchWindow or 3
-   o.maxTime = t * 60
+   o.maxTime = t * 300
    o.stopOnWalk = true
+   o.stopOnAim = true
    o.stopOnRun = true
-   o.forceProgressBar = false
    return o
 end
-
-
-
---[[ 
-Commands.SimpleSportMod.DropPoint = function(player, args)
-    local playerId = player:getOnlineID();
-    sendServerCommand("SimpleSportMod", "DropPoint", {id = playerId, dropPointX =  args.dropPointX, dropPointY =  args.dropPointY, dropPointZ =  args.dropPointZ})
-end
- ]]
--- media/lua/client/SimpleSportMod_Core.lua
+++ media/lua/client/SimpleSportMod_Core.lua
@@ -25,7 +25,210 @@
    ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
 █████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
 
------------------------            ---------------------------
+
+
+SimpleSportMod = SimpleSportMod or {}
+
+SimpleSportMod.SportItems = {
+   ["Base.SimpleSportMod_Baseball"] = true,
+   ["Base.SimpleSportMod_Basketball"] = true,
+   ["Base.SimpleSportMod_GolfBall"] = true,
+   ["Base.SimpleSportMod_TennisBall"] = true,
+   ["Base.SimpleSportMod_Football"] = true,
+   ["Base.SimpleSportMod_PoolBall"] = true,
+   ["Base.SimpleSportMod_SoccerBall"] = true,
+}
+
+SimpleSportMod.VanillaSportItems = {
+   ["Base.Baseball"] = true,
+   ["Base.Basketball"] = true,
+   ["Base.GolfBall"] = true,
+   ["Base.TennisBall"] = true,
+   ["Base.Football"] = true,
+   ["Base.Football2"] = true,
+   ["Base.PoolBall"] = true,
+   ["Base.SoccerBall"] = true,
+}
+
+SimpleSportMod.VanillaToMods = {
+   ["Base.Baseball"]    = "Base.SimpleSportMod_Baseball",
+   ["Base.Basketball"]  = "Base.SimpleSportMod_Basketball",
+   ["Base.GolfBall"]    = "Base.SimpleSportMod_GolfBall",
+   ["Base.TennisBall"]  = "Base.SimpleSportMod_TennisBall",
+   ["Base.Football"]    = "Base.SimpleSportMod_Football",
+   ["Base.Football2"]   = "Base.SimpleSportMod_Football",
+   ["Base.PoolBall"]    = "Base.SimpleSportMod_PoolBall",
+   ["Base.SoccerBall"]  = "Base.SimpleSportMod_SoccerBall",
+}
+-----------------------
+function SimpleSportMod.init(plNum, pl)
+   if isIngameState() then
+      pl:getModData()['SimpleSportMod'] = pl:getModData()['SimpleSportMod'] or {}
+      pl:getModData()['SimpleSportMod']['isPreparedToCatch'] = false
+   end
+end
+Events.OnCreatePlayer.Add(SimpleSportMod.init)
+-----------------------
+function SimpleSportMod.isSportItem(item)
+   local fType = SimpleSportMod.getFType(item)
+   return fType and SimpleSportMod.SportItems[fType] or false
+end
+
+function SimpleSportMod.getFType(item)
+   if not item then return nil end
+   return (item.getFullType and item:getFullType())
+       or (item.getWorldItem and item:getWorldItem() and item:getWorldItem():getFullType())
+end
+-----------------------
+function SimpleSportMod.getSportItem(sq)
+   sq = sq or getPlayer():getCurrentSquare()
+   if not sq then return nil end
+
+   local worldObjects = sq:getWorldObjects()
+   for i = 0, worldObjects:size() - 1 do
+      local obj = worldObjects:get(i)
+      local item = obj and obj:getItem()
+
+      if item and SimpleSportMod.isSportItem(item) then
+         item = SimpleSportMod.convert(item) 
+         local fType = SimpleSportMod.getFType(item) or item:getFullType()
+         if fType then print(fType) end
+         return item, obj, sq
+      end
+   end
+   return nil
+end
+
+function SimpleSportMod.findSportItem(sq)
+   local pl = getPlayer()
+   local rad = SandboxVars.SimpleSportMod.PickUpDistance or 3
+   sq = sq or pl:getCurrentSquare()
+   if not sq then return nil end
+   
+   local cx, cy, cz = sq:getX(), sq:getY(), sq:getZ()
+   local closestItem, closestObj, closestSq
+   local bestDist = math.huge
+
+   for dx = -rad, rad do
+      for dy = -rad, rad do
+         local checkSq = getCell():getGridSquare(cx + dx, cy + dy, cz)
+         if checkSq then
+            local worldObjects = checkSq:getWorldObjects()
+            for i = 0, worldObjects:size() - 1 do
+               local obj = worldObjects:get(i)
+               local item = obj and obj:getItem()
+
+               if item and SimpleSportMod.isSportItem(item) then
+                  item = SimpleSportMod.convert(item) 
+
+                  local px, py = pl:getX(), pl:getY()
+                  local sx, sy = checkSq:getX(), checkSq:getY()
+                  local dist = (px - sx) ^ 2 + (py - sy) ^ 2
+
+                  if dist < bestDist then
+                     bestDist = dist
+                     closestItem, closestObj, closestSq = item, obj, checkSq
+                  end
+               end
+            end
+         end
+      end
+   end
+
+   return closestItem, closestObj, closestSq
+end
+-----------------------
+function SimpleSportMod.convert(item, fType)
+   fType = fType or SimpleSportMod.getFType(item)
+   if not item or not fType then return item end
+
+   if SimpleSportMod.VanillaToMods[fType] == nil or fType == SimpleSportMod.VanillaToMods[fType] then
+      return item
+   end
+
+   local sq = item:getWorldItem() and item:getWorldItem():getSquare()
+   if not sq then return item end
+   
+   local newItem = sq:AddWorldInventoryItem(SimpleSportMod.VanillaToMods[fType], 0, 0, 0)
+   if not newItem then return item end
+   
+   newItem:setCondition(item:getCondition())
+   newItem:setName(item:getName())
+   
+   item:getWorldItem():removeFromSquare()
+   return newItem
+end
+-----------------------
+function SimpleSportMod.keypress(key)
+   if not isIngameState() then return end
+   local pl = getPlayer()
+   local catchMode = key == getCore():getKey("Catch") 
+   local pickupMode = key == getCore():getKey("Pick Up") 
+   
+   if catchMode or pickupMode then
+      local item, obj, sq = SimpleSportMod.findSportItem(pl:getSquare())   
+
+      if tostring(WeaponType.getWeaponType(pl)) ~= "barehand" then
+         local pr = pl:getPrimaryHandItem()
+         if pr then
+            ISTimedActionQueue.add(ISUnequipAction:new(pl, pr, 1))
+         end
+      end
+
+      if catchMode then
+         --print('catchMode')      
+         SimpleSportMod.instaCatchAction(pl)
+      end
+
+      if pickupMode and item then
+         --print('pickupMode')    
+         local fType = item:getFullType()
+         ISTimedActionQueue.add(ISWalkToTimedAction:new(pl, sq))
+
+         if SimpleSportMod.SportItems[fType] then
+            ISTimedActionQueue.add(ISGrabItemAction:new(pl, item:getWorldItem(), 20))
+            ISTimedActionQueue.add(ISEquipWeaponAction:new(pl, item, 20, true))
+         end
+      end
+   end
+
+   return key
+end
+Events.OnKeyPressed.Add(SimpleSportMod.keypress)
+-----------------------
+function SimpleSportMod.setReachMarker()
+   local rad = SandboxVars.SimpleSportMod.PickUpDistance or 3
+   local pl = getPlayer()
+   local sq = pl:getCurrentSquare() 
+   if SimpleSportMod.ReachMarker then SimpleSportMod.ReachMarker:remove() end
+   SimpleSportMod.ReachMarker = getWorldMarkers():addGridSquareMarker("circle_highlight_2", "circle_highlight_2", sq, 0, 0, 0.31, true, rad)
+end
+-----------------------
+function SimpleSportMod.instaCatchAction(pl)
+   pl = pl or getPlayer()
+
+   if not pl:getCharacterActions():isEmpty() then
+      pl:StopAllActionQueue()
+   end
+
+   if tostring(WeaponType.getWeaponType(pl)) ~= "barehand" then
+      local pr = pl:getPrimaryHandItem()
+      if pr then
+         ISTimedActionQueue.add(ISUnequipAction:new(pl, pr, 1))
+      end
+   end
+   
+   ISTimedActionQueue.add(ISWaitForCatchAction:new(pl))
+end
+
+function SimpleSportMod.clearCatchVar()
+   local pl = getPlayer() 
+   pl:clearVariable("waitFinished")
+   pl:clearVariable("isCatchWaiting")
+   if SimpleSportMod.ReachMarker then SimpleSportMod.ReachMarker:remove() end
+end
+--[[ -----------------------            ---------------------------
+
 SimpleSportMod = SimpleSportMod or {}
 
 SimpleSportMod.SportItems = {
@@ -168,34 +371,26 @@ function SimpleSportMod.keypress(key)
    local item, obj, sq = nil, nil, nil
    
    if catchMode or pickupMode then
-      item, obj, sq = SimpleSportMod.findSportItem(pl:getSquare())
-   end
-   if not item then      
-      return key
-   end
-
-   if catchMode then
-      SimpleSportMod.setCatchPrepare(pl, true)
-      print('catchMode')
-
-      --SimpleSportMod.instaCatchAction(pl)
-   end
-
-   if pickupMode then
-
-      print('pickupMode')
-    
-
-      local fType = item:getFullType()
-
-      ISTimedActionQueue.add(ISWalkToTimedAction:new(pl, sq))
-
+      item, obj, sq = SimpleSportMod.findSportItem(pl:getSquare())   
       if tostring(WeaponType.getWeaponType(pl)) ~= "barehand" then
          local pr = pl:getPrimaryHandItem()
          if pr then
             ISTimedActionQueue.add(ISUnequipAction:new(pl, pr, 1))
          end
       end
+   end
+
+
+   if catchMode then
+      print('catchMode')      
+      --SimpleSportMod.instaCatchAction(pl)
+      SimpleSportMod.instaCatchAction(pl)
+   end
+
+   if pickupMode then
+      print('pickupMode')    
+      local fType = item:getFullType()
+      ISTimedActionQueue.add(ISWalkToTimedAction:new(pl, sq))
 
       if SimpleSportMod.SportItems[fType] then
          ISTimedActionQueue.add(ISGrabItemAction:new(pl, item:getWorldItem(), 20))
@@ -207,6 +402,9 @@ function SimpleSportMod.keypress(key)
 end
 Events.OnKeyPressed.Add(SimpleSportMod.keypress)
 
+ ]]
+-----------------------            ---------------------------
+--[[ 
 function SimpleSportMod.FollowDropPoint(pl)
    pl = pl or getPlayer()
 
@@ -236,7 +434,6 @@ function SimpleSportMod.FollowDropPoint(pl)
       local dist = pl:DistTo(x, y)
       local newInside = dist <= rad
 
-      -- Remove and redraw marker if color changes or position changes
       if newInside ~= isInside or x ~= px or y ~= py or z ~= pz then
          if marker then marker:remove() end
          marker = createMarker(x, y, z, newInside)
@@ -247,3 +444,5 @@ function SimpleSportMod.FollowDropPoint(pl)
 
    Events.OnTick.Add(updateMarker)
 end
+ ]]
+
